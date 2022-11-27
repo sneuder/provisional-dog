1b4d79330095a0f1f193f091c4884d8b8cdc7c1c
Esneider Ospina Oviedo <sneuder8@gmail.com>
2022-11-27 18:11:32 -0500

    dataname changed

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
+    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/puppies`,
     {
         logging: false,
         native: false,
